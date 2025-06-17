9ec83fe546e29a6ece6021482ef0a5626059187d
Hidy <roiyeon@gmail.com>
2025-06-17 20:34:25 +0900

    📁 Chore: 프로젝트 MVVM 폴더 구조 초기 설정

---
+++ SpaceNote/Views/MainSpaceView.swift
@@ -0,0 +1,18 @@
+//
+//  MainSpaceView.swift
+//  SpaceNote
+//
+//  Created by yunsly on 6/17/25.
+//
+
+import SwiftUI
+
+struct MainSpaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainSpaceView()
+}
