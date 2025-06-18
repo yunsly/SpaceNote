8b0ae96992b660d4374a765d65958fc213eb8b85
Hidy <roiyeon@gmail.com>
2025-06-18 09:53:53 +0900

    ✨ Feat. 디버깅용 흔들기 삭제 기능 추가

--- SpaceNote/Views/MainSpaceView.swift
+++ SpaceNote/Views/MainSpaceView.swift
@@ -10,14 +10,45 @@ import SwiftUI
 struct MainSpaceView: View {
     // 네비게이션 로직 관리
     @EnvironmentObject var navigationManager: NavigationManager
+    @ObservedObject var viewModel: StarPointViewModel
     
     var body: some View {
         NavigationStack(path: $navigationManager.path) {
-            ZStack {
-                Image("SpaceBackground")
-                    .resizable(resizingMode: .tile)
-                    .ignoresSafeArea()
-                
+            GeometryReader { geometry in
+                ZStack {
+                    Image("SpaceBackground")
+                        .resizable(resizingMode: .tile)
+                        .ignoresSafeArea()
+                    
+                    ForEach(viewModel.stars) { star in
+                        GlowingStar(position: star.position) { newPosition in
+                            viewModel.updatePosition(for: star, to: newPosition)
+                        }
+                    }
+                    
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                viewModel.addRandomStar(in: geometry.size)
+                            }) {
+                                Image(systemName: "plus")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(Circle().fill(Color.blue))
+                            }
+                            .padding()
+                            
+                        }
+                    }
+                    
+                    ShakeDetector {
+                        viewModel.deleteAllStars()
+                    }
+                    .allowsHitTesting(false)
+                }
             }
             
         }
@@ -25,7 +56,4 @@ struct MainSpaceView: View {
     
 }
 
-#Preview {
-    MainSpaceView()
-        .environmentObject(NavigationManager())
-}
+
